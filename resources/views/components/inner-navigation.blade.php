ece4c9aa6e140cf917d9062b2730952391d23b82
Seifobeid2020 <61078719+Seifobeid2020@users.noreply.github.com>
2022-03-06 20:19:53 +0200

    fix nav-route

--- resources/views/components/inner-navigation.blade.php
+++ resources/views/components/inner-navigation.blade.php
@@ -4,13 +4,13 @@
         <a href="{{ route(Request::segment(1).'.entries.index') }}" class=" hover hover-3 "> الأدخالات </a>
     </div>
     <div class="inner_item p-3 bd-highlight {{ Request::segment(2) === 'products-log' ? 'active' : null }}">
-        <a href="products-log" class=" hover hover-3"> سجل اللوازم </a>
+        <a href="{{ route(Request::segment(1).'.products-log.index') }}" class=" hover hover-3"> سجل اللوازم </a>
     </div>
     <div class="inner_item p-3 bd-highlight {{ Request::segment(2) === 'outputs' ? 'active' : null }}">
-        <a href="outputs" class=" hover hover-3"> المخرجات </a>
+        <a href="{{ route(Request::segment(1).'.outputs.index') }}" class=" hover hover-3"> المخرجات </a>
     </div>
     <div class="inner_item p-3 bd-highlight {{ Request::segment(2) === 'custodies' ? 'active' : null }}">
-        <a href="custodies" class=" hover hover-3"> سجل العهد </a>
+        <a href="{{ route(Request::segment(1).'.custodies.index') }}" class=" hover hover-3"> سجل العهد </a>
     </div>
 
 </div>
